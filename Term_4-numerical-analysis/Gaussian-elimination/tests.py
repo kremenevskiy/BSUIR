0550371af46d3c73699c1b899a03cb95f7d642f9
kremenevskiy <kremeneuski@gmail.com>
2021-03-17 00:21:27 +0300

    Tests сheck for correctness

---
+++ Term_4-numerical-analysis/Gaussian-elimination/tests.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+A = np.array([[2, 1, -1],
+              [-3, -1, 2],
+              [-2, 1, 2]], dtype=float)
+
+b = np.array([8, -11, -3], dtype=float)
+
+
+A_1 = np.array([[1, -1],
+                [2, 1]], dtype=float)
+
+b_1 = np.array([-5, -7], dtype=float)
+
+
+A_2 = np.array([[3, 2, -5],
+                [2, -1, 3],
+                [1, 2, -1]], dtype=float)
+
+b_2 = np.array([-1, 13, 9], dtype=float)
+
+
+A_3 = np.array([[4, 2, -1],
+                [5, 3, -2],
+                [3, 2, -3]], dtype=float)
+
+b_3 = np.array([1, 2, 0], dtype=float)
+
+
+A_4 = np.array([[8, 7, 3],
+                [-7, -4, -4],
+                [-6, 5, -4]], dtype=float)
+
+b_4 = np.array([18, -11, -15], dtype=float)
+
+
+A_5 = np.array([[2, 5, 4, 1],
+                [1, 3, 2, 1],
+                [2, 10, 9, 7],
+                [3, 8, 9, 2]], dtype=float)
+
+b_5 = np.array([20, 11, 40, 37], dtype=float)
+
+
+A_6 = np.array([[0, 3, 4],
+                [2, -8, -5],
+                [-3, 2, 1]], dtype=float)
+
+b_6 = np.array([18, -37, 16], dtype=float)
