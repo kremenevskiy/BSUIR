95a23958c368e66dedea303ec57419c645877760
kremenevskiy <kremeneuski@gmail.com>
2021-06-07 02:58:35 +0300

    tests done

--- Term_4-python/python-serializer/main.py
+++ Term_4-python/python-serializer/main.py
@@ -7,10 +7,19 @@ lst = ['first', 'second', 3, {'number': 4}]
 tupl = ('test_tuple', {'status': 'ok'}, ('nested_tuple', 'one_more'))
 
 
+def func(number: int):
+    i = 10
+    res = 1
+    while i > 0:
+        i -= 1
+        res += res
+    return res
 
 
 if __name__ == '__main__':
     test_parsers(dic)
     test_parsers(lst)
     test_parsers(tupl)
+    test_parsers(func)
+
 
