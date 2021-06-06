f4ed7e779562f544097eb7054e4d06960ea1ddfa
kremenevskiy <kremeneuski@gmail.com>
2021-06-07 02:30:29 +0300

    added tests

--- Term_4-python/python-serializer/main.py
+++ Term_4-python/python-serializer/main.py
@@ -4,9 +4,13 @@ from parsers_test import test_parsers
 
 dic = {'lab': 2, 'status': 'done'}
 lst = ['first', 'second', 3, {'number': 4}]
+tupl = ('test_tuple', {'status': 'ok'}, ('nested_tuple', 'one_more'))
+
+
 
 
 if __name__ == '__main__':
     test_parsers(dic)
     test_parsers(lst)
+    test_parsers(tupl)
 
