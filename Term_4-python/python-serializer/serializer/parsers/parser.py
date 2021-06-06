1be905a7c23448ef296325b69f2fa05210dc9412
kremenevskiy <kremeneuski@gmail.com>
2021-06-07 01:57:42 +0300

    common parser

---
+++ Term_4-python/python-serializer/serializer/parsers/parser.py
@@ -0,0 +1,19 @@
+from serializer.parsers.json.json import JsonParser
+from serializer.parsers.yaml.yaml import YamlParser
+from serializer.parsers.toml.toml import TomlParser
+
+
+class Parser:
+    class ParserFactory:
+
+        @staticmethod
+        def create_parser(name: str):
+            name = name.lower()
+            if name == "json":
+                return JsonParser()
+            elif name == "yaml":
+                return YamlParser()
+            elif name == "toml":
+                return TomlParser()
+            else:
+                raise ValueError
