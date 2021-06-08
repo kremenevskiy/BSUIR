a481f954c85f7c967b2e8877120662f85a422852
kremenevskiy <kremeneuski@gmail.com>
2021-06-08 18:55:49 +0300

    created modelManager

--- Term_4-python/e-shop/shop/mainapp/models.py
+++ Term_4-python/e-shop/shop/mainapp/models.py
@@ -14,6 +14,10 @@ from django.urls import reverse
 User = get_user_model()
 
 
+def get_models_for_count(*model_names):
+    return [models.Count(model_name) for model_name in model_names]
+
+
 def get_product_url(obj, viewname):
     ct_model = obj.__class__._meta.model_name
     return reverse(viewname, kwargs={'ct_model': ct_model, 'slug': obj.slug})
@@ -48,6 +52,25 @@ class LatestProductsManager:
 class LatestProducts:
     objects = LatestProductsManager()
 
+
+class CategoryManager(models.Manager):
+    CATEGORY_NAME_COUNT_NAME = {
+        'Notebooks': 'notebook__count',
+        'Smartphones': 'smartphone__count'
+    }
+
+    def get_queryset(self):
+        return super().get_queryset()
+
+    def get_categories_for_left_sidebar(self):
+        models = get_models_for_count('notebook', 'smartphone')
+        qs = list(self.get_queryset().annotate(*models).values())
+        data = [
+            dict(name=c['name'], slug=c['slug'], count=c[self.CATEGORY_NAME_COUNT_NAME[c['name']]])
+            for c in qs
+        ]
+        return data
+
 # Create your models here.
 
 # models:
@@ -65,6 +88,7 @@ class Category(models.Model):
 
     name = models.CharField(max_length=255, verbose_name='Category Name')
     slug = models.SlugField(unique=True)
+    objects = CategoryManager()
 
     def __str__(self):
         return self.name
