87a20b054c0982fbe10919311fbf1253914f37d9
kremenevskiy <kremeneuski@gmail.com>
2021-06-08 11:51:36 +0300

    can't upload image with min resolution

--- Term_4-python/e-shop/shop/mainapp/admin.py
+++ Term_4-python/e-shop/shop/mainapp/admin.py
@@ -1,12 +1,37 @@
 from django.contrib import admin
-from django.forms import ModelChoiceField
+from django.forms import ModelChoiceField, ModelForm, ValidationError
 
 # Register your models here.
 
 from .models import *
 
+from PIL import Image
+
+
+class NotebookAdminForm(ModelForm):
+
+    MIN_RESOLUTION = (400, 400)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['image'].help_text = "Upload image with min resolution {}x{}".format(
+            *self.MIN_RESOLUTION
+        )
+
+    def clean_image(self):
+        image = self.cleaned_data['image']
+        img = Image.open(image)
+        min_height, min_width = self.MIN_RESOLUTION
+        if img.height < min_height or img.width < min_width:
+            raise ValidationError('Resolution of image less then min accpeted resolution')
+        # print(img.width, img.height)
+        return image
+
+
 
 class NotebookAdmin(admin.ModelAdmin):
+
+    form = NotebookAdminForm
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
