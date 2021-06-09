6e3a8b479b91c4b4d0e056c9e51f776877c87e67
kremenevskiy <kremeneuski@gmail.com>
2021-06-09 03:43:21 +0300

    created form and template for order

--- Term_4-python/e-shop/shop/mainapp/views.py
+++ Term_4-python/e-shop/shop/mainapp/views.py
@@ -6,6 +6,8 @@ from django.contrib import messages
 
 from .models import Notebook, Smartphone, Category, LatestProducts, Customer, Cart, CartProduct
 from .mixins import CategoryDetailMixin, CartMixin
+
+from .forms import OrderForm
 # Create your views here.
 
 
@@ -131,3 +133,17 @@ class CartView(CartMixin, View):
             'categories': categories
         }
         return render(request, 'cart.html', context)
+
+
+class CheckoutView(CartMixin, View):
+    def get(self, request, *args, **kwargs):
+
+        categories = Category.objects.get_categories_for_left_sidebar()
+        form = OrderForm(request.POST or None)
+        context = {
+            'cart': self.cart,
+            'categories': categories,
+            'form': form
+        }
+        return render(request, 'checkout.html', context)
+
--- Term_4-python/e-shop/shop/shop/settings.py
+++ Term_4-python/e-shop/shop/shop/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mainapp'
+    'mainapp',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -127,6 +128,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static_dev'),
 )
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
