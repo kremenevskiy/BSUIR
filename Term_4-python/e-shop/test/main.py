52a422bc033ddce1ee79947cde06cbb434541135
kremenevskiy <kremeneuski@gmail.com>
2021-06-14 19:43:23 +0300

    multiprocessing test

--- Term_4-python/e-shop/test/main.py
+++ Term_4-python/e-shop/test/main.py
@@ -1,4 +1,8 @@
 from threading import *
+from multiprocessing import Process
+import os
+import math
+
 
 
 class GreetHi(Thread):
@@ -18,5 +22,40 @@ t2 = GreetHello()
 
 t1.start()
 t2.start()
+
+
+# other staff
+def calc():
+    for i in range(0, 4000000):
+        math.sqrt(i)
+
+
+threads = []
+
+
+for i in range(os.cpu_count()):
+    print('registered thread %d' % i)
+    threads.append(Thread(target=calc))
+
+
+for thread in threads:
+    thread.start()
+
+for thread in threads:
+    thread.join()
+
+
+# multiprocessing
+processes = []
+
+for i in range(os.cpu_count()):
+    print('registered process %d' % i)
+    threads.append(Process(target=calc))
+
+for process in processes:
+    process.start()
+
+for process in processes:
+    process.join()
 
 
