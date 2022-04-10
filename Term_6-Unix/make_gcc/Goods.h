8bb19a8b497a4c69081db026e11e35552a80d6a7
kremenevskiy <kremeneuski@gmail.com>
2022-04-10 21:32:03 +0300

    gcc_make_file for unix

---
+++ Term_6-Unix/make_gcc/Goods.h
@@ -0,0 +1,13 @@
+#ifndef C_LANG_GOODS_H
+#define C_LANG_GOODS_H
+
+struct Good{
+    char name[20];
+    char clientID[20];
+    int value;
+    int money;
+    char date[20];
+    char timeKeeping[20];
+};
+
+#endif //C_LANG_GOODS_H
