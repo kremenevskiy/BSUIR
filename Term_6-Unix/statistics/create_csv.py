d96f4e0e16091a0cfd7808a2e7ce2579ceb402c1
kremenevskiy <kremeneuski@gmail.com>
2022-04-10 21:27:51 +0300

    work with scv file in shell scripts

--- Term_6-Unix/statistics/create_csv.py
+++ Term_6-Unix/statistics/create_csv.py
@@ -1,8 +1,11 @@
 import pandas as pd
 
-ids = ['iphone8', 'iphone9', 'iphoneX', 'iphoneXs']
-cnt = [10, 20, 30, 40]
-price = [500, 700, 900, 1000]
+ids = ['iphone', 'iphone', 'TV', 'Xbox', 'Mouse']
+cnt = [10, 20, 30, 40, 40]
+price = [500, 700, 900, 1000, 100]
 
+# print(len(ids))
+# print(len(cnt))
+# print(len(price))
 df = pd.DataFrame({'name': ids, 'count': cnt, 'price': price})
 df.to_csv('data.csv', index=False)
